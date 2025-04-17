e806ef444bb37e5a04126d3ae353945966ecff9d
Mordecai456Br <gabrielseixas1@gmail.com>
2025-04-17 01:04:22 -0300

    Update !guia.js

--- !guia.js
+++ !guia.js
@@ -1,6 +1,10 @@
-` 
-Objetivo do software: Treinar habilidades diversas de programacao por meio
-da codificacao em si e anotacoes. Essas anotacoes serao displayadas em
+// Introducao 000
+`
+Objetivo do software: Treinar habilidades diversas de programacao por meio da codificacao em si e anotacoes. Essas anotacoes serao displayadas em
 boxes contendo o conteudo, autor, data de criacao e modificacao.
+`
+// Escopo 100 
+`[ESCOPO 100 - FUNCOES]
+
 `
 
