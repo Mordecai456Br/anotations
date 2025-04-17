f6f4bbb46b95b666bc3df7391cb8d5f2d936b727
Mordecai456Br <gabrielseixas1@gmail.com>
2025-04-17 00:50:28 -0300

    Iniciando

---
+++ !guia.js
@@ -0,0 +1,6 @@
+` 
+Objetivo do software: Treinar habilidades diversas de programacao por meio
+da codificacao em si e anotacoes. Essas anotacoes serao displayadas em
+boxes contendo o conteudo, autor, data de criacao e modificacao.
+`
+
